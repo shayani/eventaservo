07c94acd676e9e30549862b0aea40851e869bdc0
Fernando Ŝajani <shayani@gmail.com>
2020-05-28 17:48:27 -0300

    JWT Token

--- app/controllers/api/v2/api_controller.rb
+++ app/controllers/api/v2/api_controller.rb
@@ -10,13 +10,13 @@ module Api
       def authorization
         return true if Rails.env.development? && params[:d].present? # DEBUG ONLY
 
-        jwt_token = request.headers['Authorization']
-        return false if jwt_token.empty?
+        jwt_token = request.headers['Authorization'].gsub(/Bearer /,"")
+        return false if jwt_token.nil?
 
         begin
           payload = JWT.decode(jwt_token, Rails.application.secrets.secret_key_base)[0]
           @user ||= User.find(payload['user_id'])
-        rescue JWT::VerificationError
+        rescue Exception
           render json: { error: 'Ne valida' }, status: 401
         end
       end
